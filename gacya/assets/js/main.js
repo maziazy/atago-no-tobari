fe4f60946acaac2b1a9e51956033ac429f595cb9
maziazy <maziazy@gmail.com>
2016-08-20 14:12:39 +0800

    Fix bug
    Fix the displaying bug that occured when the banner image is already
    loaded

--- gacya/assets/js/main.js
+++ gacya/assets/js/main.js
@@ -10,18 +10,16 @@ function initial() {
 		var message = hime.name + ' (' + hime.gacyaChance + '%)';
 		var bannerSelector	= (gacya.bannerToggled)? "#banner .front": "#banner .back";
 		var bannerPath		= _IMG_PATH + hime.banner + '.png';
-		gacya.bannerToggled = !gacya.bannerToggled;
 		
 		// Toggle banner after image is loaded
 		var bannerImage = new Image();
 		$(bannerSelector).css("background-image", 'url('+bannerPath+')');
 		bannerImage.onload = function() {
+			gacya.bannerToggled = !gacya.bannerToggled;
 			$("#gacya .message").text(message);
 			$("#banner").toggleClass("flipped");
 		};
 		bannerImage.src = bannerPath;
-		if (bannerImage.complete) bannerImage.onload();
-		
 	});
 }
 
