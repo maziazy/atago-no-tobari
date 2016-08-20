c1cab05ca408187d1bd13dfb4a94d1329f5b3e0a
maziazy <maziazy@gmail.com>
2016-08-20 13:13:56 +0800

    Add banner image
    New gacya shows banner images instead

--- gacya/assets/js/main.js
+++ gacya/assets/js/main.js
@@ -1,14 +1,28 @@
+var _IMG_PATH = "./assets/images/hime/"
 
 function initial() {
 	gacya.initial();
+	gacya.bannerToggled = 0;
 	
 	// event
-	document.getElementById("goldGacya").onclick = function() {
+	$("#goldGacya").bind("click", function() {
 		var hime = gacya.gacya();
-		var message = hime.name + ' (' + hime.gacyaChance + '%)'; 
+		var message = hime.name + ' (' + hime.gacyaChance + '%)';
+		var bannerSelector	= (gacya.bannerToggled)? "#banner .front": "#banner .back";
+		var bannerPath		= _IMG_PATH + hime.banner + '.png';
+		gacya.bannerToggled = !gacya.bannerToggled;
 		
-		document.getElementById("message").textContent = message;
-	};
+		// Toggle banner after image is loaded
+		var bannerImage = new Image();
+		$(bannerSelector).css("background-image", 'url('+bannerPath+')');
+		bannerImage.onload = function() {
+			$("#gacya .message").text(message);
+			$("#banner").toggleClass("flipped");
+		};
+		bannerImage.src = bannerPath;
+		if (bannerImage.complete) bannerImage.onload();
+		
+	});
 }
 
 window.onload = initial;
